2271d15f67a2355f1627cabdb6925bbf6c394827
Ömer Alper Güzel <90072860+omeralperguzel@users.noreply.github.com>
2022-10-14 00:05:17 +0300

    new repo created, add 3 files

---
+++ CMPE223/HandsOnActivity/HOA3/LinkedListRemoveOdds1.java
@@ -0,0 +1,42 @@
+package CMPE223.HandsOnActivity.HOA3;
+
+import java.io.*;
+
+public class Node {
+    int item;
+    Node next;
+    Node(int d)
+    {
+        item = d;
+        next = null;
+    }
+    public int getElement() {
+        return 0;
+    }
+}
+ 
+public class SLinkedList {
+    private Node head = null;
+
+    public void removeOdds() {
+        Node current = head;
+        Node previous=null;
+        while ((current.getNext()) != null) {
+            if (head == null) {
+                System.out.println("The list is empty");
+            } else {
+                while (current != null && (current.getElement()) % 2 != 1) {
+                    previous = current;
+                    current = current.getNext();
+                    System.out.println("While condition loop entered");
+                    System.out.println("current element " + current.getElement()+" previous element "+ previous.getElement());
+                }
+                if ((current.getElement()) % 2 == 1) {
+                     System.out.println("if condition met ");
+                    previous.setNext(current.getNext());
+                    System.out.println("current element " + current.getElement()+" previous element "+ previous.getElement());
+    
+                } 
+            }
+        } 
+}
