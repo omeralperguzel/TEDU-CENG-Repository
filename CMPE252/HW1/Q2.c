34b4a0fe7aa18052a54f6729db66b36addb58949
Ömer Alper Güzel <90072860+omeralperguzel@users.noreply.github.com>
2023-04-24 14:24:08 +0300

    Create Q2.c

---
+++ CMPE252/HW1/Q2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float a, b, c;
+    float x, xn_prev, xn_next;
+    int min_xn, max_xn;
+    
+    // Read input values
+    printf("Enter value of a: ");
+    scanf("%f", &a);
+    printf("Enter value of b: ");
+    scanf("%f", &b);
+    printf("Enter value of c: ");
+    scanf("%f", &c);
+    printf("Enter minimum value of x_n: ");
+    scanf("%d", &min_xn);
+    printf("Enter maximum value of x_n: ");
+    scanf("%d", &max_xn);
+    
+    // Initialize x_n_0 and x_n_1
+    xn_prev = x = 10;
+    xn_next = a * x * x + b * x + c;
+    
+    // Print x values within range
+    for (int n = 2; n <= max_xn; n++) {
+        if (n >= min_xn) {
+            printf("x_%d: %.2f\n", n, x);
+        }
+        x = xn_next;
+        xn_next = a * x * x + b * x + c;
+    }
+    
+    return 0;
+}
