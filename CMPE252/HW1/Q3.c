220b0b13e8862a1edde12aed5a1b5474fe2b9914
Ömer Alper Güzel <90072860+omeralperguzel@users.noreply.github.com>
2023-04-24 14:45:24 +0300

    Update Q3.c

--- CMPE252/HW1/Q3.c
+++ CMPE252/HW1/Q3.c
@@ -1,13 +1,13 @@
 // Declare the required header file
 
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
 // Define the Pi
 
 #define PI 3.14159
 
-// Create a g(x) fucntion
+// Create a g(x) function
 
 double gx(double x) {
   // return g(x) = x^2sin(x)
@@ -47,7 +47,6 @@ if(F=='g'){
   trapaproximation = (h / 2) * (gx(a) + gx(b) + 2 * trapsum);
   // return the area_approximation
   return trapaproximation;
-
 }
 
 // otherwise
@@ -67,36 +66,34 @@ else {
 
 // Create a main function of the program
 
-int main(int argc, char *argv[])
-
-{
-
-// Declare the required variables
-
-int n;
-
-// Display the g(x) result
-
-printf("TRAPEZOIDAL RULE g(x) = x^2sin(x), a = 0, b = PI(3.14159)\n");
-
-// for loop to display the result from 2 to 128
-
-for (n = 2; n <= 128; n *= 2){
-  // Display the trapezoidal result
-  // for n of 2 to 128
-  printf("for n = %d, area is approximately %.4f\n", n, trap(n, 0, PI, 'g'));
-}
-
-printf("\n");
-
-// Display the result for h(x)
-printf("TRAPEZOIDAL RULE h(x) = sqrt(4 - x^2), a = -2, b = 2\n");
-
-for (n = 2; n <= 128; n *= 2){
-  // Display the result from n of 2 t0 128
-  printf("for n = %d, area is approximately %.4f\n", n, trap(n, -2, 2, 'h'));
-}
-
+int main(int argc, char *argv[]) {
+  
+  // Declare the required variables
+  
+  int n;
+  
+  // Display the g(x) result
+  
+  printf("TRAPEZOIDAL RULE g(x) = x^2sin(x), a = 0, b = PI(3.14159)\n");
+  
+  // for loop to display the result from 2 to 128
+  
+  for (n = 2; n <= 128; n *= 2){
+    // Display the trapezoidal result
+    // for n of 2 to 128
+    printf("for n = %d, area is approximately %.4f\n", n, trap(n, 0, PI, 'g'));
+  }
+  
+  printf("\n");
+  
+  // Display the result for h(x)
+  printf("TRAPEZOIDAL RULE h(x) = sqrt(4 - x^2), a = -2, b = 2\n");
+  
+  for (n = 2; n <= 128; n *= 2){
+    // Display the result from n of 2 t0 128
+    printf("for n = %d, area is approximately %.4f\n", n, trap(n, -2, 2, 'h'));
+  }
+  
   return 0;
 
 }
